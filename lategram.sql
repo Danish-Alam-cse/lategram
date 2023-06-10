3468d99a2efea308fd84abd061bbf56af891e997
Md Danish Alam <danishalam002@gmail.com>
2023-06-10 21:31:10 +0530

    Added db file

--- lategram.sql
+++ lategram.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2023 at 06:41 PM
+-- Generation Time: Jun 10, 2023 at 05:59 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -43,8 +43,36 @@ INSERT INTO `posts` (`post_id`, `content`, `user_id`, `post_time`, `image`) VALU
 (10, 'Unification of Italy', 3, '2023-06-05 15:55:16', 'gm.jpg'),
 (14, 'Ji Sir', 2, '2023-06-02 16:21:50', 'sum.jpg'),
 (15, 'There is no Maulvi profession in islam.', 1, '2023-06-03 16:17:25', 'israr.jpg'),
-(17, 'Marne se pahle aye musalmaa(n)\r\nkarle tu ispe ghaur.\r\n\r\nBaabo(n) ka Khuda koi aur hai,\r\nKitabo(n) ka Khuda koi aur.', 1, '2023-06-03 16:21:03', 'ali2.jpg'),
+(17, 'Baabe te shay hi koi nhi', 1, '2023-06-07 16:19:55', 'ali.jpg'),
 (18, 'One Woman Army', 1, '2023-06-03 16:18:35', 'dr.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Juweria Fatima', 'jf@gmail.com', '786'),
+(2, 'Sumaiya Mirza', 's@gmail.com', '123'),
+(3, 'Nishat Bevinahalli', 'n@gmail.com', '456'),
+(4, 'Md Danish Alam', 'd@gmail.com', '11709'),
+(5, 'Md Danish Alam', 'n@gmail.com', ''),
+(6, 'Md Danish Alam', 'd@gmail.com', '12345'),
+(7, 'Danish', 'ds@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(8, 'Troller', 'j@gmail.com', 'b735b0c78e12553e91397a3ff19f8fd1'),
+(9, 'Sumaiya Mirza', 'sum@gmail.com', '2dafe49f49980672921f8b07071537cb');
 
 --
 -- Indexes for dumped tables
@@ -58,6 +86,12 @@ ALTER TABLE `posts`
   ADD KEY `user_post` (`user_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,6 +100,12 @@ ALTER TABLE `posts`
 --
 ALTER TABLE `posts`
   MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
